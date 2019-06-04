9e352ce702abcf2278fcc8dcdf4f186f08cfe57b
blom <blo3m123@gmail.com>
2019-06-04 22:03:34 +0200

    initial source commit

---
+++ QuickShop.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Harmony12;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace QuickShop
+{
+    static class Main
+    {
+        public static bool enabled;
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+            modEntry.OnToggle = OnToggle;
+
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            modEntry.Logger.Log("Starting QuickShop");
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(GameScript))]
+    [HarmonyPatch("Update")]
+    public class RotationInput_Update_Patcher
+    {
+        [HarmonyPostfix]
+        static void Postfix()
+        {
+            if (Input.GetKeyUp(KeyCode.B))
+            {
+                string id;
+                GameScript.Get().GetSelectedPartToMount();
+                id = GameScript.Get().GetPartMouseOver().GetIDWithTuned();
+                if (id != null)
+                {
+                    Inventory.Get().Add(id, 1f, Color.black, true, true);
+                    new NewInventoryItem(id, true).Condition = 1f;
+                    int price = Singleton<GameInventory>.Instance.GetItemProperty(id).Price;
+                    GlobalData.AddPlayerMoney(-price);
+                    UIManager.Get().ShowPopup("QiuckShop:", "Part cost: " + Helper.MoneyToString((float)price), PopupType.Buy);
+                }
+            }
+        }
+    }
+}
